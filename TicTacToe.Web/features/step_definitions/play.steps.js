64df62511258df0bd13af03e455aa2feac3276fb
TiGruu <TiGruu@users.noreply.github.com>
2014-11-26 18:18:49 +0100

    update bdd steps
    Using "global" prefix at dependencies in order to load the viewmodel in
    step definitions (beurk)

--- TicTacToe.Web/features/step_definitions/play.steps.js
+++ TicTacToe.Web/features/step_definitions/play.steps.js
@@ -2,31 +2,31 @@
 
 module.exports = function () {
 
-    var ko = require('../../js/knockout.js');
+    global.window = {};
+    global.ko = require('../../js/knockout.js');
     var app = require('../../js/vuemodele.js');
 
     var assert = require('assert');
     //var assert = require('karma-jasmine');
 
     this.Given(/^The game has started$/, function (callback) {
-        callback.pending();
-
-        var game = new app.TicTacToe();
-
-        //callback();
+        var game = new global.window.TicTacToe();
+        assert.equal(game.playcases[0].player(), null, 'The game is not at the inital state.');
+        callback();
     });
 
     this.When(/^The first player play$/, function (callback) {
         callback.pending();
+        //TODO
         //callback();
     });
 
     this.Then(/^The hand switch to the adversary$/, function (callback) {
         callback.pending();
-
-        //assert.equal(myList.getAll().length, 1, 'Grocery List should grow by one item.');
-        //assert.equal(1, 1, 'Grocery List should grow by one item.');
-        //assert.expect(1).toBe(1);
+        //TODO
+        //assert.equal(myList.getAll().length, 1, '');
+        //assert.equal(1, 1, '');
+        //assert.expect(1).toBe(1); // jasmine-based
         //callback();
     });
 
@@ -80,7 +80,7 @@ module.exports = function () {
 
 //    scenario.Then(/^The hand switch to the adversary$/, function (callback) {
 //        // Verify the expected outcome
-//        //assert.equal(1, 1, 'Grocery List should grow by one item.');
+//        //assert.equal(1, 1, '');
 
 //        // Move to next step
 //        callback();
